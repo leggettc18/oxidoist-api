b37b026e783cfcb21f1f1d61dc3f946a85c839e7
Christopher Leggett <chris@leggett.dev>
2021-02-14 20:15:07 -0500

    Early work on TodoistAPI object and return type objects.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,46 @@
+use serde::{Serialize, Deserialize};
+use reqwest::header;
+
+const base_url: &str = "https://api.todoist.com/rest/v1/";
+
+#[derive(Debug)]
+enum TodoistAPIError {
+    Error(reqwest::Error),
+    InvalidHeaderValue(reqwest::header::InvalidHeaderValue)
+}
+
+#[derive(Debug)]
+struct TodoistAPI{
+    client: reqwest::Client
+}
+
+impl TodoistAPI {
+    fn new(token: &str) -> Result<TodoistAPI, TodoistAPIError> {
+        let mut headers = header::HeaderMap::new();
+        let header_token_value = header::HeaderValue::from_str(token).map_err(TodoistAPIError::InvalidHeaderValue)?;
+        headers.insert(header::AUTHORIZATION, header_token_value);
+        let client = reqwest::Client::builder()
+            .default_headers(headers)
+            .build().map_err(TodoistAPIError::Error)?;
+        return Ok(TodoistAPI{ client })
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Project {
+    id: u32,
+    name: String,
+    color: u32,
+    parent_id: u32,
+    order: u32,
+    comment_count: u32,
+    shared: bool,
+    favorite: bool,
+    inbox_project: bool,
+    team_inbox: bool,
+    sync_id: u32
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
