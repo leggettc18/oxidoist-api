dc02eca5ecef0f6023d62d36657b45b9e0f9be86
Christopher Leggett <chris@leggett.dev>
2021-02-17 21:17:07 -0500

    Adds ability to get active tasks from Todoist API.
    This required writing a custom serializer/deserializer from
    Todoist's JSON Response to chrono::NaiveDate.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use serde::{Serialize, Deserialize};
 use reqwest::header;
 use reqwest::Client;
 use url::Url;
+use chrono::{DateTime, NaiveDate , Utc};
 
 const BASE_URL: &str = "https://api.todoist.com/rest/v1/";
 
@@ -56,6 +57,16 @@ impl TodoistAPI {
             .await.map_err(TodoistAPIError::Error)?;
         return Ok(project)
     }
+
+    pub async fn get_tasks(&self) -> Result<Vec<Task>, TodoistAPIError> {
+        let url = self.base_url.join("tasks").map_err(TodoistAPIError::UrlParseError)?;
+        let task = self.client.get(url)
+            .send()
+            .await.map_err(TodoistAPIError::Error)?
+            .json::<Vec<Task>>()
+            .await.map_err(TodoistAPIError::Error)?;
+        return Ok(task)
+    }
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -75,6 +86,61 @@ pub struct Project {
     #[serde(default)]
     team_inbox: bool,
     sync_id: u32
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Task {
+    id: u64,
+    assignee: Option<u32>,
+    comment_count: u32,
+    completed: bool,
+    content: String,
+    due: Option<Due>,
+    label_ids: Vec<u32>,
+    order: u32,
+    priority: u32,
+    project_id: u32,
+    section_id: u32,
+    parent_id: Option<u32>,
+    url: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Due {
+    #[serde(with="todoist_date_format")]
+    date: NaiveDate,
+    recurring: bool,
+    datetime: Option<DateTime<Utc>>,
+    string: String,
+    timezone: Option<String>,
+}
+
+mod todoist_date_format {
+    use chrono::{NaiveDate};
+    use serde::{self, Deserialize, Serializer, Deserializer};
+
+    const FORMAT: &'static str = "%Y-%m-%d";
+
+    pub fn serialize<S>(
+        date: &NaiveDate,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let s = format!("{}", date.format(FORMAT));
+        serializer.serialize_str(&s)
+    }
+
+    pub fn deserialize<'de, D>(
+        deserializer: D,
+    ) -> Result<NaiveDate, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        NaiveDate::parse_from_str(&s, FORMAT).map_err(serde::de::Error::custom)
+    }
 }
 
 #[cfg(test)]
