50213a1470324179857c98ce0f4b24b598708339
Christopher Leggett <chris@leggett.dev>
2021-02-19 21:56:56 -0500

    Adds ability to get tasks belonging to a specific project

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-use serde::{Serialize, Deserialize};
+use chrono::{DateTime, NaiveDate, Utc};
 use reqwest::header;
 use reqwest::Client;
+use serde::{Deserialize, Serialize};
 use url::Url;
-use chrono::{DateTime, NaiveDate , Utc};
 
 const BASE_URL: &str = "https://api.todoist.com/rest/v1/";
 
@@ -12,11 +12,11 @@ pub enum TodoistAPIError {
     Error(reqwest::Error),
     InvalidHeaderValue(reqwest::header::InvalidHeaderValue),
     InvalidHeaderName(reqwest::header::InvalidHeaderName),
-    UrlParseError(url::ParseError)
+    UrlParseError(url::ParseError),
 }
 
 #[derive(Debug)]
-pub struct TodoistAPI{
+pub struct TodoistAPI {
     base_url: Url,
     client: Client,
 }
@@ -28,86 +28,124 @@ impl TodoistAPI {
         let mut headers = header::HeaderMap::new();
         let mut bearer_token: String = "Bearer ".to_string();
         bearer_token.push_str(&token);
-        let header_token_value = header::HeaderValue::from_str(&bearer_token).map_err(TodoistAPIError::InvalidHeaderValue)?;
-        headers.insert(header::HeaderName::from_bytes(b"Authorization").map_err(TodoistAPIError::InvalidHeaderName)?, header_token_value);
+        let header_token_value = header::HeaderValue::from_str(&bearer_token)
+            .map_err(TodoistAPIError::InvalidHeaderValue)?;
+        headers.insert(
+            header::HeaderName::from_bytes(b"Authorization")
+                .map_err(TodoistAPIError::InvalidHeaderName)?,
+            header_token_value,
+        );
         let client = reqwest::Client::builder()
             .default_headers(headers)
-            .build().map_err(TodoistAPIError::Error)?;
-        return Ok(TodoistAPI{ base_url, client })
+            .build()
+            .map_err(TodoistAPIError::Error)?;
+        return Ok(TodoistAPI { base_url, client });
     }
 
     #[allow(dead_code)]
     pub async fn get_projects(&self) -> Result<Vec<Project>, TodoistAPIError> {
-        let url = self.base_url.join("projects").map_err(TodoistAPIError::UrlParseError)?;
-        let projects = self.client.get(url)
+        let url = self
+            .base_url
+            .join("projects")
+            .map_err(TodoistAPIError::UrlParseError)?;
+        let projects = self
+            .client
+            .get(url)
             .send()
-            .await.map_err(TodoistAPIError::Error)?
+            .await
+            .map_err(TodoistAPIError::Error)?
             .json::<Vec<Project>>()
-            .await.map_err(TodoistAPIError::Error)?;
+            .await
+            .map_err(TodoistAPIError::Error)?;
         return Ok(projects);
     }
 
     #[allow(dead_code)]
-    pub async fn get_project(&self, id: u32) -> Result<Project, TodoistAPIError> {
-        let url = self.base_url.join("projects/").map_err(TodoistAPIError::UrlParseError)?.join(format!("{}", id).as_str()).map_err(TodoistAPIError::UrlParseError)?;
-        let project = self.client.get(url)
+    pub async fn get_project(&self, id: u64) -> Result<Project, TodoistAPIError> {
+        let url = self
+            .base_url
+            .join("projects/")
+            .map_err(TodoistAPIError::UrlParseError)?
+            .join(format!("{}", id).as_str())
+            .map_err(TodoistAPIError::UrlParseError)?;
+        let project = self
+            .client
+            .get(url)
             .send()
-            .await.map_err(TodoistAPIError::Error)?
+            .await
+            .map_err(TodoistAPIError::Error)?
             .json::<Project>()
-            .await.map_err(TodoistAPIError::Error)?;
-        return Ok(project)
+            .await
+            .map_err(TodoistAPIError::Error)?;
+        return Ok(project);
     }
 
     pub async fn get_tasks(&self) -> Result<Vec<Task>, TodoistAPIError> {
-        let url = self.base_url.join("tasks").map_err(TodoistAPIError::UrlParseError)?;
-        let task = self.client.get(url)
+        let url = self
+            .base_url
+            .join("tasks")
+            .map_err(TodoistAPIError::UrlParseError)?;
+        let task = self
+            .client
+            .get(url)
             .send()
-            .await.map_err(TodoistAPIError::Error)?
+            .await
+            .map_err(TodoistAPIError::Error)?
             .json::<Vec<Task>>()
-            .await.map_err(TodoistAPIError::Error)?;
-        return Ok(task)
+            .await
+            .map_err(TodoistAPIError::Error)?;
+        return Ok(task);
+    }
+
+    pub async fn get_project_tasks(&self, id: u64) -> Result<Vec<Task>, TodoistAPIError> {
+        let tasks = self.get_tasks().await?;
+        let project_tasks: Vec<Task> = tasks
+            .into_iter()
+            .filter(|task| task.project_id == id)
+            .collect();
+        return Ok(project_tasks);
     }
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Project {
-    id: u32,
+    id: u64,
     name: String,
-    color: u32,
+    color: u64,
     #[serde(default)]
-    parent_id: u32,
+    parent_id: u64,
     #[serde(default)]
-    order: u32,
-    comment_count: u32,
+    order: u64,
+    comment_count: u64,
     shared: bool,
     favorite: bool,
     #[serde(default)]
     inbox_project: bool,
     #[serde(default)]
     team_inbox: bool,
-    sync_id: u32
+    sync_id: u64,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Task {
     id: u64,
-    assignee: Option<u32>,
-    comment_count: u32,
+    assignee: Option<u64>,
+    comment_count: u64,
     completed: bool,
     content: String,
     due: Option<Due>,
-    label_ids: Vec<u32>,
-    order: u32,
-    priority: u32,
-    project_id: u32,
-    section_id: u32,
-    parent_id: Option<u32>,
+    label_ids: Vec<u64>,
+    order: u64,
+    priority: u64,
+    project_id: u64,
+    section_id: u64,
+    parent_id: Option<u64>,
     url: String,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Due {
-    #[serde(with="todoist_date_format")]
+    #[serde(with = "todoist_date_format")]
     date: NaiveDate,
     recurring: bool,
     datetime: Option<DateTime<Utc>>,
@@ -116,15 +154,12 @@ pub struct Due {
 }
 
 mod todoist_date_format {
-    use chrono::{NaiveDate};
-    use serde::{self, Deserialize, Serializer, Deserializer};
+    use chrono::NaiveDate;
+    use serde::{self, Deserialize, Deserializer, Serializer};
 
     const FORMAT: &'static str = "%Y-%m-%d";
 
-    pub fn serialize<S>(
-        date: &NaiveDate,
-        serializer: S,
-    ) -> Result<S::Ok, S::Error>
+    pub fn serialize<S>(date: &NaiveDate, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
@@ -132,9 +167,7 @@ mod todoist_date_format {
         serializer.serialize_str(&s)
     }
 
-    pub fn deserialize<'de, D>(
-        deserializer: D,
-    ) -> Result<NaiveDate, D::Error>
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<NaiveDate, D::Error>
     where
         D: Deserializer<'de>,
     {
